69d777431f9ffae3ccc7b7f6266655a9664d58f2
a.gorbunov <a.gorbunov@kt-team.de>
2022-06-02 14:35:07 +0400

    the end of customization admin panel

--- app/core/models.py
+++ app/core/models.py
@@ -22,6 +22,7 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
